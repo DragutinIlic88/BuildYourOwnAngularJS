52e9a954e4e52c6ffa044880295c650a240e1dfe
Dragutin Ilic <dragutinilic88@gmail.com>
2018-04-06 16:36:27 +0200

    Value based dirty checking added

--- src/scope.js
+++ src/scope.js
@@ -1,60 +1,72 @@
 /* jshint globalstrict: true */
-'use strict';
+"use strict";
 
 //function will be assigned as initial last value of every watcher
-function initWatchVal(){}
+function initWatchVal() {}
 
-function Scope(){
-    //$$ for private variables in angualar
-    this.$$watchers = [];
-    this.$$lastDirtyWatch = null; //for short-circuiting optimization
+function Scope() {
+  //$$ for private variables in angualar
+  this.$$watchers = [];
+  this.$$lastDirtyWatch = null; //for short-circuiting optimization
 }
 
-Scope.prototype.$watch = function(watchFn, listenerFn){
-    var watcher = {
-        watchFn: watchFn,
-        listenerFn: listenerFn || function(){},
-        last: initWatchVal
-    };
-    this.$$watchers.push(watcher);
-    //disabling optimazition in case that listener of some watch add another watch
-    this.$$lastDirtyWatch = null;
+Scope.prototype.$$areEqual = function(newValue, oldValue, valueEq){
+    if(valueEq){
+        return _.isEqual(newValue, oldValue);
+    }else {
+        return newValue === oldValue;
+    }
+};
+
+//creates new watcher and push it in $$watchers array
+Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
+  var watcher = {
+    watchFn: watchFn,
+    listenerFn: listenerFn || function() {},
+    valueEq: !!valueEq, //coercing variable to a real booolean by negating it twice
+    last: initWatchVal
+  };
+  this.$$watchers.push(watcher);
+  //disabling optimazition in case that listener of some watch add another watch
+  this.$$lastDirtyWatch = null;
 };
 
 //digest trough watches once and return dirty if some watch return new value
-Scope.prototype.$$digestOnce = function(){
-    //self has this of scope
-    var self = this;
-    var newValue, oldValue, dirty;
-    _.forEach(this.$$watchers, function(watcher){
-        newValue = watcher.watchFn(self);
-        //first time $digest is called oldValue will be undefined
-        oldValue = watcher.last;
-        if(newValue !== oldValue){
-            //we now know which is last dirty watcher
-            self.$$lastDirtyWatch = watcher;
-            //here we add last property to the watcher object and assign it new value
-            watcher.last = newValue;
-            watcher.listenerFn(newValue, 
-                oldValue===initWatchVal ? newValue: oldValue, 
-                self);
-            dirty = true;
-        } else if(self.$$lastDirtyWatch === watcher){
-            return false;
-        }
-    });
-    return dirty;
+Scope.prototype.$$digestOnce = function() {
+  //self has this of scope
+  var self = this;
+  var newValue, oldValue, dirty;
+  _.forEach(this.$$watchers, function(watcher) {
+    newValue = watcher.watchFn(self);
+    //first time $digest is called oldValue will be undefined
+    oldValue = watcher.last;
+    if (!self.$$areEqual(newValue,oldValue,watcher.valueEq)) {
+      //we now know which is last dirty watcher
+      self.$$lastDirtyWatch = watcher;
+      //here we add last property to the watcher object and assign it new value
+      watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+      watcher.listenerFn(
+        newValue,
+        oldValue === initWatchVal ? newValue : oldValue,
+        self
+      );
+      dirty = true;
+    } else if (self.$$lastDirtyWatch === watcher) {
+      return false;
+    }
+  });
+  return dirty;
 };
 
 //call $$digestOnce until dirty is true
-Scope.prototype.$digest = function(){
-    var ttl = 10; //time to live is 10 iteration
-    var dirty;
-    this.$$lastDirtyWatch = null;
-    do{
-        dirty = this.$$digestOnce();
-        if(dirty && !(ttl--)){
-            throw "10 digest iterations reached";
-        }
-    }while(dirty);
+Scope.prototype.$digest = function() {
+  var ttl = 10; //time to live is 10 iteration
+  var dirty;
+  this.$$lastDirtyWatch = null;
+  do {
+    dirty = this.$$digestOnce();
+    if (dirty && !(ttl--)) {
+      throw "10 digest iterations reached";
+    }
+  } while (dirty);
 };
